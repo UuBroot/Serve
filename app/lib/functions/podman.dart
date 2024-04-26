a24a1efa65337e7295e34fc11677573704709a47
UuBroot <lolylyt@gmail.com>
2024-04-26 08:23:03 +0200

    removed unnesseseary print in podman

--- app/lib/functions/podman.dart
+++ app/lib/functions/podman.dart
@@ -59,7 +59,6 @@ Future<int> getPodmanContainerStatus(name) async {
       lineName = line.split(" ")[0].substring(1);
       if (lineName.toString() == "serve-${name}".toString()) {
         //checks if the container is in the list
-        print(line);
         String status = line.split(' ')[1]; //gets the status of the container
         switch (status) {
           case "Up":
