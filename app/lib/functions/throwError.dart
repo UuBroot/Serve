dccdb478af874379ad65da5b785fa44d803d33a3
UuBroot <lolylyt@gmail.com>
2024-05-07 15:52:00 +0200

    added error thrower

---
+++ app/lib/functions/throwError.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void showError(BuildContext context, String message) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Error'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text('Ok'),
+          ),
+        ],
+      );
+    },
+  );
+}
